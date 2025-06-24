40ac6be624afd9f674dd4bf799c3e227add48a5b
alineuwera <uwerairakozealine@gmail.com>
2025-06-24 16:20:29 +0200

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,12 @@ export default function HomePage() {
   return (
     <div className="text-white">
       <NavigationBar />
-      <div className="bg-[#267180] opacity-85 p-6">
+      <div className="bg-[#267180] opacity-80 p-6">
         <div className="flex items-center justify-between mb-4 mx-auto py-15 mt-20 px-4 sm:px-6 lg:px-8  max-w-7xl">
           <HeroSearchForm />
-          <h1 className="text-5xl font-bold mb-4 ml-10 mx-auto">
+          {/* <h1 className="text-5xl font-bold mb-4 ml-10 mx-auto">
             Easy way to find the right car
-          </h1>
+          </h1> */}
         </div>
       </div>
       <CarTypesSection />
